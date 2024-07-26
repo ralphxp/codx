0943a454b09c66b6d24656d40e7710e1683aded5
ralphxp <raphapanchi@gmail.com>
2024-07-26 20:12:49 +0100

    update 26/07 01

--- views/admin/auth/login.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('admin.layout')
-
-@section('content')
-<div id="root"></div>
-@endsection
-
-@section('scripts')
-<script type="module" src="http://localhost:5173/views/js/components/admin/auth/layout.jsx"></script>
-@endsection
--- views/admin/dashboard/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('admin.layout')
-
-@section('content')
-<div id="auth"></div>
-@endsection
-
-@section('scripts')
-<script type="module" src="http://localhost:5173/views/js/components/admin/layout.jsx"></script>
-@endsection
